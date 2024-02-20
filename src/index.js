83b7c398b2af1fd4cfda56741c4cdc12c86cff0f
Travis Horn <travis@travishorn.com>
2024-02-20 15:53:27 -0600

    Store latest imported tx to eliminate duplicates

--- src/index.js
+++ src/index.js
@@ -1,40 +1,86 @@
 #!/usr/bin/env node
 
-import { readFile } from "node:fs/promises";
+import { readFile, appendFile, writeFile } from "node:fs/promises";
+import { parse as pathParse, join } from "node:path";
 import { program } from "commander";
 import { DateTime } from "luxon";
 import Papa from "papaparse";
 import { parseCurrencyString, parseAmount, plainText } from "./lib.js";
 
-async function main(txFilePath, rulesFilePath) {
+async function main(txFilePath, rulesFilePath, journalFilePath) {
+  // Read the transactions from CSV file (provided by financial institution)
   const txString = await readFile(txFilePath, "utf-8");
+
+  // Read the rules that define how to parse the transactions (written by user)
   const rules = JSON.parse(await readFile(rulesFilePath, "utf-8"));
+
+  // If user didn't specify a time zone, assume UTC
   const timeZone = rules.timeZone ?? "utc";
 
+  // Given an index number, return the field name in the rules file
   function transformHeader(_, i) {
     return rules.fields[i];
   }
 
+  // Parse the CSV transactions. Skip empty lines, recognize the header, and
+  // transform the header column (property) names according to the fields in the
+  // rules file
   const parsed = Papa.parse(txString, {
     skipEmptyLines: true,
     header: true,
     transformHeader,
   });
 
+  // If there were errors parsing, throw an error to discontinue the process
   if (parsed.errors.length > 0) {
     throw new Error(JSON.stringify(parsed.errors));
   }
 
-  const transformed = parsed.data
+  // Transactions are listed latest-first in the source CSV file. Reverse them
+  // so the order is consistent with journal when appended (oldest-first)
+  const reversed = parsed.data.reverse();
+
+  // Determine the file path to the rules file
+  const rulesFileParts = pathParse(rulesFilePath);
+
+  // Build a file path for the "latest" file. This file will be named the same
+  // as the rules file, but with the extension ".latest" instead of ".json". Its
+  // purpose is to store the most recently imported transaction
+  const latestFilePath = join(
+    rulesFileParts.dir,
+    `${rulesFileParts.name}.latest`,
+  );
+
+  // Get the latest imported transaction, if it exists.
+  let latestTx;
+  try {
+    latestTx = await readFile(latestFilePath, "utf-8");
+  } catch (err) {}
+
+  // Find that same transaction in the CSV transaction file
+  const latestTxIndex = reversed.findIndex(
+    (tx) => JSON.stringify(tx) === latestTx,
+  );
+
+  // Remove any transactions that are on or before that latest transaction.
+  // Those have already been imported so they should be ignored.
+  const toBeImported =
+    latestTxIndex >= 0 ? reversed.slice(latestTxIndex + 1) : reversed;
+
+  const transformed = toBeImported
     .map((tx) => {
+      // Transform the date into an ISO date, trim the description, and parse
+      // the amount into a number
       const output = {
         date: DateTime.fromFormat(tx.date, rules.dateFormat, {
           zone: timeZone,
         }).toISODate(),
-        description: tx.description,
+        description: tx.description.trim(),
         amount: parseAmount(tx, rules.locale, rules.currency),
       };
 
+      // If the file contains balance information, parse that into a number, as
+      // well.
       if (tx.balance) {
         output.balance = parseCurrencyString(
           tx.balance,
@@ -46,16 +92,25 @@ async function main(txFilePath, rulesFilePath) {
       return output;
     })
     .map((tx) => {
+      // Set the payee equal to the description (for now) and set the accounts
+      // to the default defined in the rules
       const output = {
         ...tx,
+        payee: tx.description,
         account1: rules.account1,
         account2: rules.account2,
       };
 
+      // Loop through the transaction rules in the rules file
       rules.txRules.forEach((rule) => {
+        // Create a regular expression from the pattern string
         const descriptionPattern = new RegExp(rule.pattern, "g");
+
+        // Look for a matching pattern in the current transaction's description
         const match = tx.description.match(descriptionPattern);
 
+        // If the description matches the pattern, set the payee, comment, and
+        // accounts according to the rule
         if (match) {
           if (rule.payee) output.payee = rule.payee;
           if (rule.comment) output.comment = rule.comment;
@@ -67,18 +122,38 @@ async function main(txFilePath, rulesFilePath) {
       return output;
     });
 
-  const pt = transformed.reduce((prev, curr) => {
+  // Build a string containing plaintext transactions. It's a very specific
+  // format for use with hledger
+  const plaintextTransactions = transformed.reduce((prev, curr) => {
     return prev + plainText(curr, rules.locale, rules.currency);
   }, "");
 
-  console.log(pt);
+  if (journalFilePath) {
+    // If the user provided a journal file path, append the transactions to the
+    // journal
+    await appendFile(journalFilePath, plaintextTransactions);
+
+    // and store the latest transaction that was just imported
+    await writeFile(
+      latestFilePath,
+      JSON.stringify(parsed.data[parsed.data.length - 1]),
+    );
+  } else {
+    // If no journal file was specified, just show the plaintext transactions
+    console.log(plaintextTransactions);
+  }
 }
 
+// Set the arguments and parse the command-line command
 program
   .argument(
     "<transactions file>",
     "Path to the CSV file containing financial transaction data",
   )
   .argument("<rules file>", "Path to the JSON file containing parsing rules.")
+  .argument(
+    "[journal file]",
+    "Path to the journal to which the transactions should be appended",
+  )
   .action(main)
   .parse();
--- src/lib.js
+++ src/lib.js
@@ -1,54 +1,107 @@
+// Takes a string representing an amount of currency, a locale, and the name of
+// a currency. Returns the amount as a number
 export function parseCurrencyString(string, locale, currency) {
+  // Use Intl NumberFormat to create a formatter for the given locale/currency
   const formatter = new Intl.NumberFormat(locale, {
     style: "currency",
     currency,
   });
 
+  // Get the parts of this currency's format
   const parts = formatter.formatToParts(0);
+
+  // Get the decimal character (usually `.` but sometimes `,`)
   const decimal = parts.find((part) => part.type === "decimal").value;
+
+  // Create a pattern that matches any **except** a digit, a negative sign, or
+  // the decimal mark
   const pattern = new RegExp(`[^0-9-${decimal}]`, "g");
+
+  // Strip everything but digits, negative sign, and decimal mark from the
+  // string
   const stripped = string.replace(pattern, "");
+
+  // Convert the decimal mark to a period `.` since JS numbers use period to
+  // indicate a decimal mark
   const normalized = stripped.replace(decimal, ".");
 
+  // Return the amount as a number
   return Number(normalized);
 }
 
+// Takes a transaction, a locale, and a currency. Returns the transaction amount
+// as a number.
 export function parseAmount(tx, locale, currency) {
+  // If there's an `amount` field, just return the amount in that.
   if (tx.amount) return parseCurrencyString(tx.amount, locale, currency);
+
+  // However, some institutions use separate columns for "amount in" and
+  // "amount out". If this transaction has something in the `amount-in` field,
+  // return that number as a negative
   if (tx["amount-in"])
     return -parseCurrencyString(tx["amount-in"], locale, currency);
+
+  // If this transaction has something in the `amount-out` field, return that
+  // number
   if (tx["amount-out"])
     return parseCurrencyString(tx["amount-out"], locale, currency);
+
+  // Otherwise, no amounts were found anywere. Return 0
   return 0;
 }
 
+// Takes a transaction object and returns it as a plaintext string used by
+// hledger.
 export function plainText(tx, locale, currency) {
+  // Attempt to line up amounts using spaces. The end of each account string
+  // will be padded with spaces
   const accountPadLen = Math.max(43, tx.account1.length, tx.account2.length);
+
+  // Format the amount in the given locale/currency
   const account1Amount = (-tx.amount).toLocaleString(locale, {
     style: "currency",
     currency,
   });
+
+  // Format the matching amount (double entry bookkeeping means theres always
+  // an opposite amount taken/given from two accounts)
   const account2Amount = tx.amount.toLocaleString(locale, {
     style: "currency",
     currency,
   });
+
+  // Attend to line up the amounts using spaces
   const amountPadLen = Math.max(account1Amount.length, account2Amount.length);
-  const balanceAmount = tx.balance.toLocaleString(locale, {
+
+  // Format the balance amount
+  const balanceAmount = tx.balance?.toLocaleString(locale, {
     style: "currency",
     currency,
   });
 
+  // Start this transaction's string with the date and payee
   let output = `${tx.date} ${tx.payee}`;
 
+  // If there's a comment, append it
   if (tx.comment) {
     output += `  ; ${tx.comment}\n`;
   } else {
     output += "\n";
   }
 
-  output +=
-    `    ${tx.account1.padEnd(accountPadLen)} ${account1Amount.padStart(amountPadLen)} = ${balanceAmount}\n` +
-    `    ${tx.account2.padEnd(accountPadLen)} ${account2Amount.padStart(amountPadLen)}\n\n`;
+  // Append the first account and its amount
+  output += `    ${tx.account1.padEnd(accountPadLen)} ${account1Amount.padStart(amountPadLen).padEnd(11)}`;
+
+  // If there's a balance, append it
+  if (tx.balance) {
+    output += `= ${balanceAmount.padStart(11)}\n`;
+  } else {
+    output += "\n";
+  }
+
+  // Append the second account and its amount, followed by two newlines so the
+  // string/file is ready for another transaction
+  output += `    ${tx.account2.padEnd(accountPadLen)} ${account2Amount.padStart(amountPadLen)}\n\n`;
 
   return output;
 }
